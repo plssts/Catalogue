10c4a348967c72d33b65cb5d9767d751e5b2debb
Paulius Staisiunas <paulius@LT-LIT-SC-0217.internal.visma.com>
2020-02-26 18:31:20 +0200

    JMS initial objects.

--- src/main/java/com/j2020/JmsConfiguration.java
+++ src/main/java/com/j2020/JmsConfiguration.java
@@ -8,11 +8,15 @@ import org.springframework.jms.config.DefaultJmsListenerContainerFactory;
 import org.springframework.jms.support.converter.MappingJackson2MessageConverter;
 import org.springframework.jms.support.converter.MessageConverter;
 import org.springframework.jms.support.converter.MessageType;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 import javax.jms.ConnectionFactory;
+import java.util.concurrent.Executor;
 
 @Configuration
 @EnableJms
+@EnableAsync
 public class JmsConfiguration {
     @Bean
     public DefaultJmsListenerContainerFactory jmsListenerContainerFactory(ConnectionFactory connectionFactory,
@@ -30,4 +34,16 @@ public class JmsConfiguration {
 
         return converter;
     }
+
+    @Bean(name = Constants.JMS_ASYNC_EXECUTOR)
+    public Executor concurrentExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setThreadNamePrefix(Constants.JMS_ASYNC_EXECUTOR + ":");
+        executor.setCorePoolSize(10);
+        executor.setMaxPoolSize(100);
+        executor.initialize();
+
+        return executor;
+    }
+
 }
